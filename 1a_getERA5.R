8eb0311ca65196a5178499d491db4e0773b28ebd
Ani Ghosh <anighosh@rstudio-server.us-central1-a.c.cgiar-bigdata-projects.internal>
2021-02-15 21:03:37 +0000

    mm

--- 1a_getERA5.R
+++ 1a_getERA5.R
@@ -49,7 +49,7 @@ qq <- data.frame(variable = c("precipitation_flux","solar_radiation_flux",rep("2
                               "10m_wind_speed", "2m_relative_humidity"),
                  statistics = c(NA, NA, "24_hour_maximum", "24_hour_mean", "24_hour_minimum",
                                 "24_hour_mean", NA),
-                 time = c(NA,NA,NA,NA,NA,NA, "15_00"))
+                 time = c(NA,NA,NA,NA,NA,NA, "12_00"))
 
 # temporal range
 years <- as.character(1979:2020)

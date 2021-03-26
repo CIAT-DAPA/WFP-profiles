7874d3a1c02af8515e7bce004288557290588cb1
haachicanoy <harold22010@gmail.com>
2021-03-26 14:03:03 -0500

    Change id type

--- _gcm_ds.R
+++ _gcm_ds.R
@@ -96,7 +96,7 @@ mergeGCMdailyTable <- function(iso, model, experiment, gcmdir, outdir, rref){
               tbl$y <- NULL
             } else {
               tbl <- df[[i]]
-              tbl$id   <- as.character(tbl$id)
+              tbl$id   <- as.numeric(tbl$id)
               tbl$date <- as.Date(tbl$date)
             }
             return(tbl)

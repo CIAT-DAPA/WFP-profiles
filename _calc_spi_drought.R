e2f2fccb2f1727ba92bbed3fb87c99fc691a3408
Cesar A. Saavedra Vanegas <31328713+cesarsaa@users.noreply.github.com>
2021-08-23 17:14:24 -0500

    root

--- _calc_spi_drought.R
+++ _calc_spi_drought.R
@@ -20,8 +20,8 @@ calc_spi_drought <- function(spi_data = infile, output = outfile, country = coun
   # root <<- '//dapadfs.cgiarad.org/workspace_cluster_13/WFP_ClimateRiskPr'
   OSys <<- Sys.info()[1]
   root <<- switch(OSys,
-                  'Linux'   = '/CATALOGUE/Workspace14/WFP_ClimateRiskPr',
-                  'Windows' = '//CATALOGUE/Workspace14/WFP_ClimateRiskPr')
+                  'Linux'   = '/dapadfs/workspace_cluster_14/WFP_ClimateRiskPr',
+                  'Windows' = '//dapadfs.cgiarad.org/workspace_cluster_14/WFP_ClimateRiskPr')
   
   if(!file.exists(outfile)){
     # Load municipalities/districts shapefile
